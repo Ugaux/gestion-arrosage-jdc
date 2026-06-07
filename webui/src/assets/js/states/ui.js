0ad3d689993b72016ab9f3e89378b9a1e87c6ac8
Ugaux <hugomuller68@yahoo.fr>
2026-06-07 18:46:53 +0200

    Handle screen sizes from JS instead of CSS

--- webui/src/assets/js/states/ui.js
+++ webui/src/assets/js/states/ui.js
@@ -11,10 +11,6 @@ export default {
     },
   },
 
-  sidebarOpen: false,
-  currentPage: "home",
-  overlayPage: null,
-
   init() {
     // Add closing of sidebar with escape key
     window.addEventListener("keydown", (e) => {
@@ -23,18 +19,20 @@ export default {
       }
     });
 
-    window.matchMedia("(min-width: 993px)").addEventListener("change", (e) => {
-      // Solves the case where sidebar would come back open when it was first opened,
-      // then window was resized to a large screen then back to a small one
+    const mediaQuery = window.matchMedia("(min-width: 993px)");
+    document.documentElement.classList.toggle("is-desktop", mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", (e) => {
+      document.documentElement.classList.toggle("is-desktop", e.matches);
       if (e.matches) {
         // Entering large screen
         if (this.sidebar.isOpen) {
+          // Solves the case where sidebar would come back open when it was first opened,
+          // then window was resized to a large screen then back to a small one
           console.log("Closing small/medium screens sidebar...");
           this.sidebar.close();
         }
       }
     });
-
-    this.sidebar.close(); //TODO: here
   },
 };
