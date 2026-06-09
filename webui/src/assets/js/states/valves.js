7201501323de123e287d2f54a5f3726b60b293bc
Ugaux <hugomuller68@yahoo.fr>
2026-06-09 21:51:24 +0200

    Rename relays.js to valves.js

--- webui/src/assets/js/states/valves.js
+++ webui/src/assets/js/states/valves.js
@@ -5,7 +5,7 @@ export default {
     { id: 3, name: "IOExpander 2", is_checked: true },
     { id: 4, name: "IOExpander 3", is_checked: false },
     { id: 5, name: "IOExpander 4", is_checked: false },
-    { id: 6, name: "IOExpander 5", is_checked: false },
+    { id: 6, name: "IOExpander 5", is_checked: true },
     { id: 7, name: "IOExpander 6", is_checked: false },
     { id: 8, name: "IOExpander 7", is_checked: false },
   ],
