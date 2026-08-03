44f30f315bd82ad21c2e8915b2a5a3926a18d830
Ugaux <hugomuller68@yahoo.fr>
2026-08-03 16:02:15 +0200

    Format ESPIniFile files

--- firmware/lib/ESPIniFile/examples/parsingWithErrors/parsingWithErrors.ino
+++ firmware/lib/ESPIniFile/examples/parsingWithErrors/parsingWithErrors.ino
@@ -1,71 +1,69 @@
 /*
-	Remeber to upload the data directory to your board!
-	
-	Serial baud rate in this example is 9600
+
+  Remember to upload the data directory to your board!
+  Serial baud rate in this example is 9600
+  
 */
 
 #include <ESPIniFile.h>
 
-void printErrorMessage(uint8_t e, bool eol = true)
-{
+void printErrorMessage(uint8_t e, bool eol = true) {
   switch (e) {
-  case ESPIniFile::errorNoError:
-    Serial.print("no error");
-    break;
-  case ESPIniFile::errorFileNotFound:
-    Serial.print("file not found");
-    break;
-  case ESPIniFile::errorFileNotOpen:
-    Serial.print("file not open");
-    break;
-  case ESPIniFile::errorBufferTooSmall:
-    Serial.print("buffer too small");
-    break;
-  case ESPIniFile::errorSeekError:
-    Serial.print("seek error");
-    break;
-  case ESPIniFile::errorSectionNotFound:
-    Serial.print("section not found");
-    break;
-  case ESPIniFile::errorKeyNotFound:
-    Serial.print("key not found");
-    break;
-  case ESPIniFile::errorEndOfFile:
-    Serial.print("end of file");
-    break;
-  case ESPIniFile::errorUnknownError:
-    Serial.print("unknown error");
-    break;
-  default:
-    Serial.print("unknown error value");
-    break;
+    case ESPIniFile::errorNoError:
+      Serial.print("no error");
+      break;
+    case ESPIniFile::errorFileNotFound:
+      Serial.print("file not found");
+      break;
+    case ESPIniFile::errorFileNotOpen:
+      Serial.print("file not open");
+      break;
+    case ESPIniFile::errorBufferTooSmall:
+      Serial.print("buffer too small");
+      break;
+    case ESPIniFile::errorSeekError:
+      Serial.print("seek error");
+      break;
+    case ESPIniFile::errorSectionNotFound:
+      Serial.print("section not found");
+      break;
+    case ESPIniFile::errorKeyNotFound:
+      Serial.print("key not found");
+      break;
+    case ESPIniFile::errorEndOfFile:
+      Serial.print("end of file");
+      break;
+    case ESPIniFile::errorUnknownError:
+      Serial.print("unknown error");
+      break;
+    default:
+      Serial.print("unknown error value");
+      break;
   }
   if (eol)
     Serial.println();
 }
 
-void setup()
-{
-  
+void setup() {
+
   const size_t bufferLen = 80;
-  char buffer[bufferLen];
+  char         buffer[bufferLen];
 
   const char *filename = "/net.ini";
   Serial.begin(9600);
-  
-  //Mount the LittleFS  
+
+  //Mount the LittleFS
   if (!LittleFS.begin())
     while (1)
       Serial.println("LittleFS.begin() failed");
-  
+
   ESPIniFile ini(filename);
   if (!ini.open()) {
     Serial.print("Ini file ");
     Serial.print(filename);
     Serial.println(" does not exist");
     // Cannot do anything else
-    while (1)
-      ;
+    while (1);
   }
   Serial.println("Ini file exists");
 
@@ -77,57 +75,48 @@ void setup()
     Serial.print(" not valid: ");
     printErrorMessage(ini.getError());
     // Cannot do anything else
-    while (1)
-      ;
+    while (1);
   }
-  
+
   // Fetch a value from a key which is present
   if (ini.getValue("network", "mac", buffer, bufferLen)) {
     Serial.print("section 'network' has an entry 'mac' with value ");
     Serial.println(buffer);
-  }
-  else {
+  } else {
     Serial.print("Could not read 'mac' from section 'network', error was ");
     printErrorMessage(ini.getError());
   }
-  
+
   // Try fetching a value from a missing key (but section is present)
   if (ini.getValue("network", "nosuchkey", buffer, bufferLen)) {
     Serial.print("section 'network' has an entry 'nosuchkey' with value ");
     Serial.println(buffer);
-  }
-  else {
+  } else {
     Serial.print("Could not read 'nosuchkey' from section 'network', error was ");
     printErrorMessage(ini.getError());
   }
-  
+
   // Try fetching a key from a section which is not present
   if (ini.getValue("nosuchsection", "nosuchkey", buffer, bufferLen)) {
     Serial.print("section 'nosuchsection' has an entry 'nosuchkey' with value ");
     Serial.println(buffer);
-  }
-  else {
+  } else {
     Serial.print("Could not read 'nosuchkey' from section 'nosuchsection', error was ");
     printErrorMessage(ini.getError());
   }
 
   // Fetch a boolean value
-  bool allowPut; // variable where result will be stored
+  bool allowPut;  // variable where result will be stored
   bool found = ini.getValue("/upload", "allow put", buffer, bufferLen, allowPut);
   if (found) {
     Serial.print("The value of 'allow put' in section '/upload' is ");
     // Print value, converting boolean to a string
     Serial.println(allowPut ? "TRUE" : "FALSE");
-  }
-  else {
+  } else {
     Serial.print("Could not get the value of 'allow put' in section '/upload': ");
     printErrorMessage(ini.getError());
   }
 }
 
-
-void loop()
-{
-
-
+void loop() {
 }
--- firmware/lib/ESPIniFile/src/ESPIniFile.h
+++ firmware/lib/ESPIniFile/src/ESPIniFile.h
@@ -9,189 +9,175 @@
 #include <LittleFS.h>
 #include <IPAddress.h>
 
-
 class ESPIniFileState;
 
 class ESPIniFile {
 public:
-	enum error_t {
-		errorNoError = 0,
-		errorFileNotFound,
-		errorFileNotOpen,
-		errorBufferTooSmall,
-		errorSeekError,
-		errorSectionNotFound,
-		errorKeyNotFound,
-		errorEndOfFile,
-		errorUnknownError,
-	};
+  enum error_t : uint8_t {
+    errorNoError = 0,
+    errorFileNotFound,
+    errorFileNotOpen,
+    errorBufferTooSmall,
+    errorSeekError,
+    errorSectionNotFound,
+    errorKeyNotFound,
+    errorEndOfFile,
+    errorUnknownError,
+  };
 
-	static const uint8_t maxFilenameLen;
+  static const uint8_t maxFilenameLen;
 
-	// Create an ESPIniFile object. It isn't opened until open() is called on it.
-	ESPIniFile(const char* filename, const char* mode = "r",
-			bool caseSensitive = false);
-	~ESPIniFile();
+  // Create an ESPIniFile object. It isn't opened until open() is called on it.
+  ESPIniFile(const char* filename, const char* mode = "r",
+             bool caseSensitive = false);
+  ~ESPIniFile();
 
-	inline bool open(void); // Returns true if open succeeded
-	inline void close(void);
+  inline bool open(void);  // Returns true if open succeeded
+  inline void close(void);
 
-	inline bool isOpen(void) const;
+  inline bool isOpen(void) const;
 
-	inline error_t getError(void) const;
-	inline void clearError(void) const;
-	// Get the file mode (FILE_READ/FILE_WRITE)
-	inline const char* getMode(void) const;
+  inline error_t getError(void) const;
+  inline void    clearError(void) const;
+  // Get the file mode (FILE_READ/FILE_WRITE)
+  inline const char* getMode(void) const;
 
-	// Get the filename asscoiated with the ini file object
-	inline const char* getFilename(void) const;
+  // Get the filename asscoiated with the ini file object
+  inline const char* getFilename(void) const;
 
-	bool validate(char* buffer, size_t len) const;
+  bool validate(char* buffer, size_t len) const;
 
-	// Get value from the file, but split into many short tasks. Return
-	// value: false means continue, true means stop. Call getError() to
-	// find out if any error
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, ESPIniFileState &state) const;
+  // Get value from the file, but split into many short tasks. Return
+  // value: false means continue, true means stop. Call getError() to
+  // find out if any error
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, ESPIniFileState& state) const;
 
-	// Get value, as one big task. Return = true means value is present
-	// in buffer
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len) const;
+  // Get value, as one big task. Return = true means value is present
+  // in buffer
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len) const;
 
-	// Get the value as a string, storing the result in a new buffer
-	// (not the working buffer)
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, char *value, size_t vlen) const;
+  // Get the value as a string, storing the result in a new buffer
+  // (not the working buffer)
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, char* value, size_t vlen) const;
 
-	// Get a boolean value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, bool& b) const;
+  // Get a boolean value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, bool& b) const;
 
-	// Get an integer value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, int& val) const;
+  // Get an integer value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, int& val) const;
 
-	// Get a uint8_t value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, uint8_t& val) const;
+  // Get a uint8_t value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, uint8_t& val) const;
 
-	// Get a uint16_t value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, uint16_t& val) const;
+  // Get a uint16_t value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, uint16_t& val) const;
 
-	// Get a long value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, long& val) const;
+  // Get a long value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, long& val) const;
 
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, unsigned long& val) const;
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, unsigned long& val) const;
 
-	// Get a float value
-	bool getValue(const char* section, const char* key,
-				  char* buffer, size_t len, float& val) const;
+  // Get a float value
+  bool getValue(const char* section, const char* key,
+                char* buffer, size_t len, float& val) const;
 
-	bool getIPAddress(const char* section, const char* key,
-					  char* buffer, size_t len, uint8_t* ip) const;
+  bool getIPAddress(const char* section, const char* key,
+                    char* buffer, size_t len, uint8_t* ip) const;
 
 #if defined(ARDUINO) && ARDUINO >= 100
-	bool getIPAddress(const char* section, const char* key,
-					  char* buffer, size_t len, IPAddress& ip) const;
+  bool getIPAddress(const char* section, const char* key,
+                    char* buffer, size_t len, IPAddress& ip) const;
 #endif
 
-	bool getMACAddress(const char* section, const char* key,
-					   char* buffer, size_t len, uint8_t mac[6]) const;
+  bool getMACAddress(const char* section, const char* key,
+                     char* buffer, size_t len, uint8_t mac[6]) const;
 
-	// Utility function to read a line from a file, make available to all
-	//static int8_t readLine(File &file, char *buffer, size_t len, uint32_t &pos);
-	static error_t readLine(File &file, char *buffer, size_t len, uint32_t &pos);
-	static bool isCommentChar(char c);
-	static char* skipWhiteSpace(char* str);
-	static void removeTrailingWhiteSpace(char* str);
+  // Utility function to read a line from a file, make available to all
+  //static int8_t readLine(File &file, char *buffer, size_t len, uint32_t &pos);
+  static error_t readLine(File& file, char* buffer, size_t len, uint32_t& pos);
+  static bool    isCommentChar(char c);
+  static char*   skipWhiteSpace(char* str);
+  static void    removeTrailingWhiteSpace(char* str);
 
-	bool getCaseSensitive(void) const;
-	void setCaseSensitive(bool cs);
+  bool getCaseSensitive(void) const;
+  void setCaseSensitive(bool cs);
 
 protected:
-	// True means stop looking, false means not yet found
-	bool findSection(const char* section, char* buffer, size_t len,
-					 ESPIniFileState &state) const;
-	bool findKey(const char* section, const char* key, char* buffer,
-				 size_t len, char** keyptr, ESPIniFileState &state) const;
-
+  // True means stop looking, false means not yet found
+  bool findSection(const char* section, char* buffer, size_t len,
+                   ESPIniFileState& state) const;
+  bool findKey(const char* section, const char* key, char* buffer,
+               size_t len, char** keyptr, ESPIniFileState& state) const;
 
 private:
-	char _filename[ESPINI_FILE_MAX_FILENAME_LEN];
-	const char* _mode;
-	mutable error_t _error;
-	mutable File _file;
-	bool _caseSensitive;
+  char            _filename[ESPINI_FILE_MAX_FILENAME_LEN];
+  const char*     _mode;
+  mutable error_t _error;
+  mutable File    _file;
+  bool            _caseSensitive;
 };
 
-bool ESPIniFile::open(void)
-{
-	if (_file)
-		_file.close();
-	_file = LittleFS.open(_filename, _mode);
-	if (isOpen()) {
-		_error = errorNoError;
-		return true;
-	}
-	else {
-		_error = errorFileNotFound;
-		return false;
-	}
+bool ESPIniFile::open(void) {
+  if (_file)
+    _file.close();
+  _file = LittleFS.open(_filename, _mode);
+  if (isOpen()) {
+    _error = errorNoError;
+    return true;
+  } else {
+    _error = errorFileNotFound;
+    return false;
+  }
 }
 
-void ESPIniFile::close(void)
-{
-	if (_file)
-		_file.close();
+void ESPIniFile::close(void) {
+  if (_file)
+    _file.close();
 }
 
-bool ESPIniFile::isOpen(void) const
-{
-	return (_file == true);
+bool ESPIniFile::isOpen(void) const {
+  return (_file == true);
 }
 
-ESPIniFile::error_t ESPIniFile::getError(void) const
-{
-	return _error;
+ESPIniFile::error_t ESPIniFile::getError(void) const {
+  return _error;
 }
 
-void ESPIniFile::clearError(void) const
-{
-	_error = errorNoError;
+void ESPIniFile::clearError(void) const {
+  _error = errorNoError;
 }
 
-const char* ESPIniFile::getMode(void) const
-{
-	return _mode;
+const char* ESPIniFile::getMode(void) const {
+  return _mode;
 }
 
-const char* ESPIniFile::getFilename(void) const
-{
-	return _filename;
+const char* ESPIniFile::getFilename(void) const {
+  return _filename;
 }
-
-
 
 class ESPIniFileState {
 public:
-	ESPIniFileState();
+  ESPIniFileState();
 
 private:
-	enum {funcUnset = 0,
-		  funcFindSection,
-		  funcFindKey,
-	};
+  enum { funcUnset = 0,
+         funcFindSection,
+         funcFindKey,
+  };
 
-	uint32_t readLinePosition;
-	uint8_t getValueState;
+  uint32_t readLinePosition;
+  uint8_t  getValueState;
 
-	friend class ESPIniFile;
+  friend class ESPIniFile;
 };
 
-
 #endif
-
